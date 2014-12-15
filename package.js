a36dbc89c7ec6c6ca3c1020e0ff14dbffa799a86
Morten Henriksen <mh@gi2.dk>
2014-12-15 20:27:45 +0100

    convert into new ps

--- package.js
+++ package.js
@@ -1,25 +1,21 @@
 Package.describe({
   name: "ground:dictionary",
-  version: "0.0.1",
+  version: "0.1.0",
   summary: "Adds a simple value based dictionary",
   git: "https://github.com/GroundMeteor/dictionary.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
 
-  api.versionsFrom && api.versionsFrom('METEOR@0.9.1');
+  api.versionsFrom('1.0');
 
   api.export('Dictionary');
 
-  api.add_files('dictionary.js', ['client', 'server']);
+  api.addFiles('dictionary.js', ['client', 'server']);
 });
 
-Package.on_test(function (api) {
-  if (api.versionsFrom) {
-    api.use('ground:dictionary', ['client', 'server']);
-  } else {
-    api.use('dictionary', ['client', 'server']);
-  }
+Package.onTest(function (api) {
+  api.use('ground:dictionary', ['client', 'server']);
   api.use('test-helpers', 'client');
   api.use(['tinytest', 'underscore', 'ejson']);
 
